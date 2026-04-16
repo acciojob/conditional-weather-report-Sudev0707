aa6214805f3fb814ae953fecd7b4e0242c1c5958
sudev <sudev1997@gmail.com>
2026-04-16 16:15:47 +0530

    weather-condition

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+
+  const [weather, setWeather] = React.useState({
+    temperature: 25,
+    conditions: "Sunny",
+  });
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
   )
 }
